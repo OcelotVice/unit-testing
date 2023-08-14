02870db1034a9e25f8896a5439ad63c0953952c3
gabriel <iamgabrielvice@gmail.com>
2023-08-14 21:44:59 +1000

    first-branch

--- funcs.js
+++ funcs.js
@@ -1,13 +1,24 @@
 function isFive(num) {
   // Your code here
+  return num == 5;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number != "number") {
+    throw new Error;
+  }
+  return number % 2 != 0;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let array = [];
+
+  for (let i = min; i <= max; i += step) {
+    array.push(i);
+  }
+  return array;
 }
 
 
